a9d5f163c531ba52a40510a7e659c8f874db895b
Gianluca Santoni <santonigl@gmail.com>
2022-05-05 14:08:12 +0200

    setting up folders

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Gianluca Santoni"
+__copyright__ = "Copyright 2022"
+__credits__ = ["Gianluca Santoni"]
+__license__ = ""
+__version__ = "0.1"
+__maintainer__ = "Gianluca Santoni"
+__email__ = "gianluca.santoni@esrf.fr"
+__status__ = "pre-alpha"
